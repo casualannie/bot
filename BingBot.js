fa1b9dcbd096965d1813d14eae477da02267db4b
casualannie <108879306+casualannie@users.noreply.github.com>
2023-06-29 19:40:59 +0300

    Научили ходить по нескольким сайтам

--- BingBot.js
+++ BingBot.js
@@ -6,23 +6,52 @@
 // @author       Sokolova Anna
 // @match        https://www.bing.com/*
 // @match        https://ortholike.ru/*
+// @match        https://www.rudenta.ru/*
+// @match        https://www.dentalfantasy.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let links = document.links;
-let search_icon = document.getElementById("search_icon");
-let keywords = [
+let sites = {
+"ortholike.ru": [   
   "Центр ортодонтии в Москве",
   "Андрей Жук клиника",
   "Самый крупный ортодонтический центр в России",
   "Здоровые и ровные зубы",
   "Элайнеры по доступной цене",
   "Виды брекет-систем",
-  "Безлигатурные брекеты: плюсы и минусы"
-];
+  "Безлигатурные брекеты: плюсы и минусы "
+],
+  "rudenta.ru": [
+  "Лечение зубов во сне с седацией",
+  "Программа Год заботы о себе",
+  "Лечение зубов под наркозом",
+  "Клиника ТОП-3 в России",
+  "Уникальные услуги лечения зубов"
+],
+"dentalfantasy.ru": [
+  "Онлайн-консультация детского стоматолога",
+  "Лидер российской детской стоматологии",
+  "Даем гарантию на пломбы до смены зубов",
+  "Стоматология Дентал Фэнтези",
+  "Клиника, где деткам хорошо!"
+]
+}
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)]; //получили случайный сайт из обьекта
+let links = document.links;
+let search_icon = document.getElementById("search_icon");
+let keywords = sites[site]; //получили набор ключевых фраз для конкретного сайта
 let keyword = keywords[getRandom(0, keywords.length)];
 let googleInput = document.getElementsByName("q")[0];
+
+//Работаем с cookie
+if (search_icon != undefined) {
+  document.cookie = "site=" + site;
+} else if (location.hostname == "www.bing.com") {
+  site = getCookie("site");
+} else {
+  site = location.hostname;
+}
 
 //Работаем на главной странице поисковика
 if (search_icon != undefined) {
@@ -37,7 +66,7 @@ if (search_icon != undefined) {
   }, 500);
 
   //работаем на целевом сайте
-} else if (location.hostname == "ortholike.ru") {
+} else if (location.hostname == site) {
   console.log("Мы на целевом сайте!");
 
   setInterval(() => {
@@ -47,8 +76,8 @@ if (search_icon != undefined) {
       location.href = "https://www.bing.com/";
     }
     if (links.length == 0) {
-      location.href = "https://ortholike.ru/";
-    } else if (links[index].href.indexOf("ortholike.ru") != -1) {
+      location.href = site;
+    } else if (links[index].href.indexOf(site) != -1) {
       links[index].click();
     }
   }, getRandom(3500, 5500));
@@ -58,7 +87,7 @@ if (search_icon != undefined) {
 } else {
   let nextBingPage = true;
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("ortholike.ru")) {
+    if (links[i].href.includes(site)) {
       let link = links[i];
       nextBingPage = false;
       console.log("Нашёл строку" + link);
@@ -70,12 +99,12 @@ if (search_icon != undefined) {
   }
   let elementExist = setInterval(() => {
     let element = document.querySelector(".sb_pagS");
-         
+   
       if (element.innerText == "5") {
         nextBingPage = false;
         location.href = "https://www.bing.com/";
         clearInterval(elementExist);
-      }
+    }
   }, 100)
 
   if (nextBingPage) {
@@ -88,4 +117,11 @@ if (search_icon != undefined) {
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
